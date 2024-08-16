12c4542f63e0b3874230eabe5526f68b642ac945
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-16 16:13:04 -0300

    cart context

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from 'react'
 
 import { Header } from '@/components/header'
+import { CartProvider } from '@/contexts/cart-context'
 
 export default function StoreLayout({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
-      <Header />
+    <CartProvider>
+      <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
+        <Header />
 
-      {children}
-    </div>
+        {children}
+      </div>
+    </CartProvider>
   )
 }
